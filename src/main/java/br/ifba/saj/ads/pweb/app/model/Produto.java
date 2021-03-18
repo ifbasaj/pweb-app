1180eb5b77fb665b238df6a73ec9bec96ee43bf0
Leandro Costa Souza <leandro.costa@ifba.edu.br>
2021-03-18 15:19:47 +0000

    add novos testes

---
+++ src/main/java/br/ifba/saj/ads/pweb/app/model/Produto.java
@@ -0,0 +1,21 @@
+package br.ifba.saj.ads.pweb.app.model;
+
+public class Produto {
+    Integer id;
+    public Integer getId() {
+     return id;
+    }
+    public void setId(Integer id) {
+     this.id = id;
+    }
+    public void getNome() {
+     
+    }
+    public void getValor() {
+        
+
+    }
+    public void getQuantidade() {
+        
+    }
+   }
--- src/test/java/br/ifba/saj/ads/pweb/app/AppTest.java
+++ src/test/java/br/ifba/saj/ads/pweb/app/AppTest.java
@@ -1,39 +1,108 @@
 package br.ifba.saj.ads.pweb.app;
 
 import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelEvaluationException;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import br.ifba.saj.ads.pweb.app.model.Produto;
+
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
-public class AppTest
-{
+public class AppTest {
     public AppTest() {
     }
 
     @Test
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    public void testProdutoModelId() {
+        
+        Produto produto = new Produto();
 
-    @Test
-    public void testMore()
-    {
-        assertTrue( true );
-    }
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp;
 
-    @Test
-    public void abrirConexao(){
         try {
-            Connection	conexao	=	DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/seJbtH8iSv","seJbtH8iSv","S00lDxLCdK");
+            exp = parser.parseExpression("id");
+            Integer id = 5;
+            exp.setValue(produto,id);
+            assertEquals(id, exp.getValue(produto));
+        } catch (SpelEvaluationException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testProdutoModelNome() {
+        
+        Produto produto = new Produto();
+
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp;
+
+        try {
+            exp = parser.parseExpression("nome");
+            String nome = "teste";
+            exp.setValue(produto,nome);
+            assertEquals(nome, exp.getValue(produto));
+        } catch (SpelEvaluationException e) {
+            fail(e.getMessage());
+        }
+
+    }
+    @Test
+    public void testProdutoModelValor() {
+        
+        Produto produto = new Produto();
+
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp;
+
+        try {
+            exp = parser.parseExpression("valor");
+            Float valor = 5.7f;
+            exp.setValue(produto,valor);
+            assertEquals(valor, exp.getValue(produto));
+        } catch (SpelEvaluationException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testProdutoModelQuantidade() {
+        
+        Produto produto = new Produto();
+
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp;
+
+        try {
+            exp = parser.parseExpression("quantidade");
+            Integer quantidade = 5;
+            exp.setValue(produto,quantidade);
+            assertEquals(quantidade, exp.getValue(produto));
+        } catch (SpelEvaluationException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void abrirConexao() {
+        try {
+            Connection conexao = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/seJbtH8iSv",
+                    "seJbtH8iSv", "S00lDxLCdK");
             conexao.close();
-            assertTrue( true );
-                
+            assertTrue(true);
+
         } catch (Exception e) {
             e.printStackTrace();
-            assertTrue( false );
+            assertTrue(false);
         }
 
     }
