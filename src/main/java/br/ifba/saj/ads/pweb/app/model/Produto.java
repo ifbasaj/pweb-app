965741af693e989362ee074231e60ff57fad0315
Leandro Costa Souza <leandro.costa@ifba.edu.br>
2021-03-18 19:07:23 +0000

    limpando produto

--- src/main/java/br/ifba/saj/ads/pweb/app/model/Produto.java
+++ src/main/java/br/ifba/saj/ads/pweb/app/model/Produto.java
@@ -1,21 +1,4 @@
 package br.ifba.saj.ads.pweb.app.model;
 
 public class Produto {
-    Integer id;
-    public Integer getId() {
-     return id;
-    }
-    public void setId(Integer id) {
-     this.id = id;
-    }
-    public void getNome() {
-     
-    }
-    public void getValor() {
-        
-
-    }
-    public void getQuantidade() {
-        
-    }
    }
